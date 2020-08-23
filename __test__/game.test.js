c7cd69eadd9c8d633671e573c512ea4c4503ce14
Shivanand Guness <shivam.guness@yahoo.com>
2020-08-23 14:05:08 +0400

    Update game.test.js
    removed spaces

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -9,7 +9,7 @@ describe('App', () => {
       done()
     })
   })
-}) 
+})
 
 describe('Game', () => {
   let game, p1, p2
@@ -17,13 +17,13 @@ describe('Game', () => {
     p1 = 'Salem'
     p2 = 'Nate'
     game = new Game(p1, p2)
-  }) 
+  })
 
   describe('Game', () => {
     it('Initializes with two players', async () => {
       expect(game.p1).toBe('Salem')
       expect(game.p2).toBe('Nate')
-    }) 
+    })
 
     it('Initializes with an empty board', async () => {
       for (let r = 0; r < game.board.length; r++) {
